09d1c159a0e3dfe9b468db722e0ff280ffd134bd
Ryan Holanda <ryanholanda08@gmail.com>
2026-03-11 10:57:47 -0300

    fix: remove base from get all supported protocols

--- src/modules/protocols/protocols.service.ts
+++ src/modules/protocols/protocols.service.ts
@@ -14,19 +14,13 @@ export class ProtocolsService {
       GetProtocolsDocument,
     );
 
-    // TODO: REMOVE HOTFIX FOR Base when the issue is resolved
-    const baseResponse = await new GraphQLClient('https://indexer.dedicated.hyperindex.xyz/0454ac3/v1/graphql').request<
-      GetProtocolsQuery,
-      GetProtocolsQueryVariables
-    >(GetProtocolsDocument);
-
     // TODO: REMOVE HOTFIX FOR Ethereum when the issue is resolved
     const ethereumResponse = await new GraphQLClient(
       'https://indexer.dedicated.hyperindex.xyz/aefe5f4/v1/graphql',
     ).request<GetProtocolsQuery, GetProtocolsQueryVariables>(GetProtocolsDocument);
 
     // TODO: REMOVE HOTFIX FOR Ethereum and Base when the issue is resolved
-    const allProtocols = [...response.Protocol, ...baseResponse.Protocol, ...ethereumResponse.Protocol];
+    const allProtocols = [...response.Protocol, ...ethereumResponse.Protocol];
     const uniqueProtocols = Array.from(new Map(allProtocols.map((p) => [p.id, p])).values());
 
     return uniqueProtocols;
